3c769906cc6475b91a70f0cf13c13df9244c3eea
Cecilie Brandt <c.brandtolsen@gmail.com>
2016-03-30 21:05:02 +0200

    ShearVisualisation initial

---
+++ K2Structural/K2Structural/ShearVisualisation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System.Linq;
+
+namespace K2Structural
+{
+    public class ShearVisualisation : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the ShearVisualisation class.
+        /// </summary>
+        public ShearVisualisation()
+          : base("ShearVisualisation", "ShearDisplay",
+              "Visualise the shear forces with colour (blue=low, green=medium, red=high)",
+              "K2Structural", "4 Display")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("{d488d4b0-242c-4aeb-bace-6c65ab059d5a}"); }
+        }
+    }
+}
