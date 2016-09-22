822c16ef6969beb9d7da9e36a4b8aa66b0e1af81
Cecilie Brandt <c.brandtolsen@gmail.com>
2016-09-22 18:38:03 +0200

    Inherit PlanktonMesh to extend the functionality

--- K2Engineering/K2Engineering/MeshVertexArea.cs
+++ K2Engineering/K2Engineering/MeshVertexArea.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Plankton;
+using System.Linq;
 using Grasshopper.Kernel;
 using Rhino.Geometry;
 
@@ -23,6 +24,8 @@ namespace K2Engineering
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
+            pManager.AddGenericParameter("PlanktonMesh", "pMesh", "PlanktonMesh in [m]", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("ProjectToXY", "proj", "Project the mesh faces to the XY plane (useful for e.g. snow load calculation)", GH_ParamAccess.item, false);
         }
 
         /// <summary>
@@ -30,6 +33,9 @@ namespace K2Engineering
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
+            pManager.AddGenericParameter("Vertices", "V", "The mesh vertices", GH_ParamAccess.list);
+            pManager.AddVectorParameter("VertexNormals", "n", "The vertex normals scaled according to the associated voronoi area [m2]", GH_ParamAccess.list);
+            pManager.AddVectorParameter("area", "a", "Areas", GH_ParamAccess.list);
         }
 
         /// <summary>
@@ -38,7 +44,50 @@ namespace K2Engineering
         /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+            //Input
+            PlanktonMesh pMesh = new PlanktonMesh();
+            DA.GetData(0, ref pMesh);
+
+            bool projXY = false;
+            DA.GetData(1, ref projXY);
+
+
+            //Calculate
+            PMeshExt pMeshE = new PMeshExt(pMesh);
+            Point3d[] verticesXYZ = pMeshE.convertVerticesToXYZ();
+            Vector3d[] vertexNormals = pMeshE.calcVertexNormals();
+
+            Vector3d[] faceNormals = new Vector3d[pMeshE.Faces.Count];
+            for(int i=0; i<pMeshE.Faces.Count; i++)
+            {
+                faceNormals[i] = pMeshE.calcFaceNormal(i);
+            }
+
+
+
+            //Output
+            DA.SetDataList(0, verticesXYZ);
+            DA.SetDataList(1, vertexNormals);
+            DA.SetDataList(2, faceNormals);
+
         }
+
+        //Methods
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         /// <summary>
         /// Provides an Icon for the component.
---
+++ K2Engineering/K2Engineering/PMeshExt.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Plankton;
+using Grasshopper;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Geometry;
+
+namespace K2Engineering
+{
+    public class PMeshExt: PlanktonMesh
+    {
+
+        public PMeshExt(PlanktonMesh source): base(source)
+        {
+
+        }
+
+
+
+        //----------------------------------------- CALCULATE FACE NORMAL-------------------------------------------//
+        //Note: calculate face normal (not normalised) as average of cross products of edge pairs (n-gon)
+
+        public Vector3d calcFaceNormal(int faceIndex)
+        {
+            //Create face edge vectors
+            int[] faceHalfedges = this.Faces.GetHalfedges(faceIndex);
+
+            Vector3d[] edgesCCW = new Vector3d[faceHalfedges.Length];
+
+            for (int i = 0; i < faceHalfedges.Length; i++)
+            {
+                int startVertex = this.Halfedges[faceHalfedges[i]].StartVertex;
+                Point3d start = new Point3d(this.Vertices[startVertex].X, this.Vertices[startVertex].Y, this.Vertices[startVertex].Z);
+                int nextHalfedge = this.Halfedges[faceHalfedges[i]].NextHalfedge;
+                int endVertex = this.Halfedges[nextHalfedge].StartVertex;
+                Point3d end = new Point3d(this.Vertices[endVertex].X, this.Vertices[endVertex].Y, this.Vertices[endVertex].Z);
+
+                edgesCCW[i] = new Vector3d(end - start);
+            }
+
+            //Shift edge vectors
+            Vector3d[] edgesCCW_shift = new Vector3d[edgesCCW.Length];
+            for (int j = 0; j < edgesCCW.Length; j++)
+            {
+                edgesCCW_shift[j] = edgesCCW[(j + 1) % edgesCCW.Length];
+            }
+
+            //Calculate face normal vector from cross product of edge vectors
+            Vector3d normal = new Vector3d(0, 0, 0);
+            for (int k = 0; k < edgesCCW.Length; k++)
+            {
+                normal += (Vector3d.CrossProduct(edgesCCW[k], edgesCCW_shift[k]) / 2.0);
+            }
+            normal = normal / edgesCCW.Length;
+
+            return normal;
+        }
+
+
+
+        //----------------------------------------- CALCULATE VERTEX NORMAL-------------------------------------------//
+        //Note: calculate vertex normal as weighted average of the adjacent face normals (not normalised)
+
+        public Vector3d calcVertexNormal(int vertexIndex)
+        {
+            int[] adjFaces = this.Vertices.GetVertexFaces(vertexIndex);
+
+            Vector3d vNormal = new Vector3d(0, 0, 0);
+            foreach (int faceIndex in adjFaces)
+            {
+                if (faceIndex != -1)
+                {
+                    vNormal += calcFaceNormal(faceIndex);
+                }
+            }
+            vNormal.Unitize();
+
+            return vNormal;
+        }
+
+
+
+        //----------------------------------------- CALCULATE VERTEX NORMALS (ALL)-------------------------------------------//
+        public Vector3d[] calcVertexNormals()
+        {
+            Vector3d[] vertexNormals = new Vector3d[this.Vertices.Count];
+
+            for (int i = 0; i < this.Vertices.Count; i++)
+            {
+                vertexNormals[i] = calcVertexNormal(i);
+            }
+
+            return vertexNormals;
+        }
+
+
+
+        //-----------------------------------------TEST IF THE MESH IS TRIANGULATED-------------------------------------------//
+        public bool isMeshTriangulated()
+        {
+            bool isTriangulated = false;
+
+            int count = 0;
+            for(int i=0; i<this.Faces.Count; i++)
+            {
+                int[] faceVertices = this.Faces.GetFaceVertices(i);
+                if(faceVertices.Length == 3)
+                {
+                    count++;
+                }
+            }
+
+            if(count == this.Faces.Count)
+            {
+                isTriangulated = true;
+            }
+
+            return isTriangulated;
+        }
+
+        
+
+        //-----------------------------------------CREATE LIST OF VERTICES AS POINT3D-------------------------------------------//
+        public Point3d[] convertVerticesToXYZ()
+        {
+            Point3d[] verticesXYZ = new Point3d[this.Vertices.Count];
+
+            for (int i = 0; i < this.Vertices.Count; i++)
+            {
+                verticesXYZ[i] = new Point3d(this.Vertices[i].X, this.Vertices[i].Y, this.Vertices[i].Z);
+            }
+
+            return verticesXYZ;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
