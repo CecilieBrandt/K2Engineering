e8779c2df090d486290a4f393acdcece17f9c1ee
Cecilie Brandt <c.brandtolsen@gmail.com>
2016-09-22 15:29:11 +0200

    Create component to calculate mesh vertex voronoi area

---
+++ K2Engineering/K2Engineering/MeshVertexArea.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace K2Engineering
+{
+    public class MeshVertexArea : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MeshVertexArea class.
+        /// </summary>
+        public MeshVertexArea()
+          : base("MeshVertexArea", "MVArea",
+              "Calculate the voronoi area associated with each vertex of a mesh",
+              "K2Eng", "2 Load")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("{9881eebc-de15-4d78-a45f-3375efbeb5a9}"); }
+        }
+    }
+}
