084c09e5379461ea96e596373df9313ac1817098
Cecilie Brandt-Olsen <c.brandtolsen@gmail.com>
2017-10-28 00:42:34 +0200

    Initial calibration attempt

--- K2Engineering/K2Engineering/Beam.cs
+++ K2Engineering/K2Engineering/Beam.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 using Grasshopper.Kernel;
 using Rhino.Geometry;
+using KangarooSolver;
 
 namespace K2Engineering
 {
@@ -23,10 +24,11 @@ namespace K2Engineering
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            pManager.AddPlaneParameter("StartPlane", "startPln", "The start plane of the beam", GH_ParamAccess.item);
-            pManager.AddPlaneParameter("EndPlane", "endPln", "The end plane of the beam", GH_ParamAccess.item);
+            pManager.AddPlaneParameter("StartPlane", "startPln", "The start plane of the beam (model in [m])", GH_ParamAccess.item);
+            pManager.AddPlaneParameter("EndPlane", "endPln", "The end plane of the beam (model in [m])", GH_ParamAccess.item);
             pManager.AddNumberParameter("E-modulus", "E", "Young's modulus in [MPa]", GH_ParamAccess.item);
             pManager.AddNumberParameter("G-modulus", "G", "The shear modulus in [MPa]", GH_ParamAccess.item);
+            pManager.AddNumberParameter("A", "A", "The cross section area in [mm2]", GH_ParamAccess.item);
             pManager.AddNumberParameter("Iy", "Iy", "The moment of inertia about the cross section y-axis in [mm4]", GH_ParamAccess.item);
             pManager.AddNumberParameter("Iz", "Iz", "The moment of inertia about the cross section z-axis in [mm4]", GH_ParamAccess.item);
             pManager.AddNumberParameter("It", "It", "The torsional moment of inertia in [mm4]", GH_ParamAccess.item);
@@ -59,18 +61,21 @@ namespace K2Engineering
             double gModulus = 0.0;
             DA.GetData(3, ref gModulus);
 
+            double area = 0.0;
+            DA.GetData(4, ref area);
+
             double inertiaY = 0.0;
-            DA.GetData(4, ref inertiaY);
+            DA.GetData(5, ref inertiaY);
 
             double inertiaZ = 0.0;
-            DA.GetData(5, ref inertiaZ);
+            DA.GetData(6, ref inertiaZ);
 
             double inertiaT = 0.0;
-            DA.GetData(6, ref inertiaT);
+            DA.GetData(7, ref inertiaT);
 
 
             //Calculate
-            GoalObject beamElement = new BeamGoal(startPln, endPln, eModulus, gModulus, inertiaY, inertiaZ, inertiaT);
+            GoalObject beamElement = new BeamGoal(startPln, endPln, eModulus, gModulus, area, inertiaY, inertiaZ, inertiaT);
 
 
             //Output
@@ -80,130 +85,205 @@ namespace K2Engineering
         //Define beam goal
         public class BeamGoal : GoalObject
         {
-            Plane P0;
-            Plane P1;
+            Plane P0;               //Start plane (local)
+            Plane P1;               //End plane (local)
+            Plane P0R;              //Updated start plane (local)
+            Plane P1R;              //Updated end plane (local)
+
             double restLength;
-            double E;
-            double G;
-            double Iy;
-            double Iz;
-            double It;
+            double E, G, A, Iy, Iz, It;
+            double thetaY0, thetaZ0, thetaY1, thetaZ1, thetaX;
 
-            //From K2 goal
-            Plane P0R;
-            Plane P1R;
+            double N, MY0, MZ0, MY1, MZ1, MX;
 
-            double TX1, TX2, TY1, TY2, twist;
 
-            public BeamGoal(Plane startPlane, Plane endPlane, double eModulus, double gModulus, double inertiaY, double inertiaZ, double inertiaT)
+            public BeamGoal(Plane startPlane, Plane endPlane, double eModulus, double gModulus, double area, double inertiaY, double inertiaZ, double inertiaT)
             {
                 PPos = new Point3d[2] {startPlane.Origin, endPlane.Origin};
                 Move = new Vector3d[2];
-                Weighting = new double[2] {1.0, 1.0};           //Units: [N*m2]
+                Weighting = new double[2] {1.0, 1.0};           
 
                 Torque = new Vector3d[2];
-                TorqueWeighting = new double[2] { 1.0, 1.0 };
+                TorqueWeighting = new double[2] {1.0, 1.0};
 
-                //Other
+                Plane startGlobal = new Plane(startPlane.Origin, Vector3d.XAxis, Vector3d.YAxis);
+                Plane endGlobal = new Plane(endPlane.Origin, Vector3d.XAxis, Vector3d.YAxis);
+                InitialOrientation = new Plane[2] { startGlobal, endGlobal };       //Needs to be global because the calculated forces and rotations in nodes are calculated globally
+
+                //Local end planes
                 P0 = startPlane;
                 P1 = endPlane;
+                P0R = P0;
+                P1R = P1;
+
+                //Translate initial local end planes to Origin
+                P0.Transform(Transform.ChangeBasis(Plane.WorldXY, startGlobal));
+                P1.Transform(Transform.ChangeBasis(Plane.WorldXY, endGlobal));
+
                 restLength = startPlane.Origin.DistanceTo(endPlane.Origin);
                 E = eModulus;
                 G = gModulus;
+                A = area;
                 Iy = inertiaY;
                 Iz = inertiaZ;
                 It = inertiaT;
-
-                //From K2 goal
-                P0.Transform(Transform.ChangeBasis(Plane.WorldXY, StartNode));
-                P1.Transform(Transform.ChangeBasis(Plane.WorldXY, EndNode));
-                P0R = startPlane;
-                P1R = endPlane;
             }
 
 
             public override void Calculate(List<KangarooSolver.Particle> p)
             {
-                Point3d P0 = p[PIndex[0]].Position;
-                Point3d P1 = p[PIndex[1]].Position;
+                //Get the current positions/orientations of the nodes (global: related to InitialOrientation)
+                Plane P0Current = p[PIndex[0]].Orientation;
+                Plane P1Current = p[PIndex[1]].Orientation;
 
-                Vector3d V01 = P1 - P0;
+                Vector3d elementVec = new Vector3d(P1Current.Origin - P0Current.Origin);
+                double currentLength = elementVec.Length;
+                Vector3d elementDir = new Vector3d(elementVec);
+                elementDir.Unitize();
 
-                //From K2 goal
-                //get the current positions/orientations of the nodes
-                Plane NodeACurrent = p[PIndex[0]].Orientation;
-                Plane NodeBCurrent = p[PIndex[1]].Orientation;
 
-                //get the initial orientations of the beam end frames..
+                //Calculate angle changes (referring to Eq. 2.1.7 to 2.1.9)
+
+                //Get the initial orientations of the end planes (local orientation sitting in Origin)
                 P0R = P0;
                 P1R = P1;
-                //..and transform them to get the current beam end frames
-                P0R.Transform(Transform.PlaneToPlane(Plane.WorldXY, NodeACurrent));
-                P1R.Transform(Transform.PlaneToPlane(Plane.WorldXY, NodeBCurrent));
 
-                //axial (ignoring elongation due to bowing for now)
-                Vector3d Current = P1R.Origin - P0R.Origin;
-                double CurrentLength = Current.Length;
-                double Stretch = CurrentLength - RestLength;
-                Vector3d AxialMove = 0.5 * (Current / CurrentLength) * Stretch;
+                //Orient initial local plane sitting in Origin from WorldXY to current global plane in node. This gives the updated local plane
+                P0R.Transform(Transform.PlaneToPlane(Plane.WorldXY, P0Current));
+                P1R.Transform(Transform.PlaneToPlane(Plane.WorldXY, P1Current));
 
-                Vector3d X1 = P0R.XAxis;
-                Vector3d Y1 = P0R.YAxis;
-                Vector3d X2 = P1R.XAxis;
-                Vector3d Y2 = P1R.YAxis;
+                //Extract vectors of updated local planes
+                Vector3d Y0 = P0R.XAxis;
+                Vector3d Z0 = P0R.YAxis;
+                Vector3d Y1 = P1R.XAxis;
+                Vector3d Z1 = P1R.YAxis;
 
-                //bend angles
-                Vector3d UnitCurrent = Current;
-                UnitCurrent.Unitize();
-                TX1 = Y1 * UnitCurrent;
-                TX2 = Y2 * UnitCurrent;
-                TY1 = X1 * UnitCurrent;
-                TY2 = X2 * UnitCurrent;
+                //Biaxial bending angle changes
+                thetaY0 = Z0 * elementDir;
+                thetaZ0 = -Y0 * elementDir;
+                thetaY1 = Z1 * elementDir;
+                thetaZ1 = -Y1 * elementDir;
 
-                //twist
-                twist = ((X1 * Y2) - (X2 * Y1)) / 2.0;
-
-                //moments
-                Vector3d Moment1 = (X1 * TX1) - (Y1 * TY1);
-                Vector3d Moment2 = (X2 * TX2) - (Y2 * TY2);
-
-                Torque[0] = -0.25 * (Moment1 + twist * Current);
-                Torque[1] = -0.25 * (Moment2 - twist * Current);
-                TorqueWeighting[0] = TorqueWeighting[1] = E * A;
-
-                //  shears
-                Vector3d SY1 = 0.25 * Vector3d.CrossProduct(TX1 * X1, Current);
-                Vector3d SX1 = 0.25 * Vector3d.CrossProduct(TY1 * Y1, Current);
-                Vector3d SY2 = 0.25 * Vector3d.CrossProduct(TX2 * X2, Current);
-                Vector3d SX2 = 0.25 * Vector3d.CrossProduct(TY2 * Y2, Current);
-
-                Move[0] = AxialMove + SX1 - SY1 + SX2 - SY2;
-                Move[1] = -Move[0];
+                //Twist angle change
+                thetaX = ((Y0 * Z1) - (Y1 * Z0)) / 2.0;
 
 
+                //Axial
+                //double extension = currentLength - restLength;
+                double extension = ((Math.Pow(elementVec.Length, 2) - Math.Pow(restLength, 2)) / (2.0 * restLength)) + ((restLength / 60.0) * (4.0 * (Math.Pow(thetaY0, 2) + Math.Pow(thetaZ0, 2)) - 2.0 * ((thetaY0 * thetaY1) - (thetaZ0 * thetaZ1)) + 4.0 * (Math.Pow(thetaY1, 2) + Math.Pow(thetaZ1, 2))));         //Unit: [m]
 
 
+                //Element internal forces (referring to 2.1.11 to 2.1.16)
+                N = ((E * A) / restLength) * extension;      // Unit: [N]
+
+                MY0 = (((N * restLength) / 30.0) * ((4.0 * thetaY0) - thetaY1)) + (((2.0 * E * Iy * 1e-6) / restLength) * ((2.0 * thetaY0) - thetaY1));          //Unit: [Nm]
+                MY1 = (((N * restLength) / 30.0) * ((4.0 * thetaY1) - thetaY0)) + (((2.0 * E * Iy * 1e-6) / restLength) * ((2.0 * thetaY1) - thetaY0));          //Unit: [Nm]
+
+                MZ0 = (((N * restLength) / 30.0) * ((4.0 * thetaZ0) - thetaZ1)) + (((2.0 * E * Iz * 1e-6) / restLength) * ((2.0 * thetaZ0) - thetaZ1));          //Unit: [Nm]
+                MZ1 = (((N * restLength) / 30.0) * ((4.0 * thetaZ1) - thetaZ0)) + (((2.0 * E * Iz * 1e-6) / restLength) * ((2.0 * thetaZ1) - thetaZ0));          //Unit: [Nm]
+
+                MX = ((G * It * 1e-6) / restLength) * thetaX;            //Unit: [Nm]
+
+
+                //To do: calculate shear forces from moments
+
+
+                //Global forces (referring to 2.1.17 to 2.1.20)
+                //Force start
+                double F0X = (1.0 / restLength) * ((N * elementVec.X) + (MY0 * Z0.X) - (MZ0 * Y0.X) + (MY1 * Z1.X) + (MZ1 * Y1.X));
+                double F0Y = (1.0 / restLength) * ((N * elementVec.Y) + (MY0 * Z0.Y) - (MZ0 * Y0.Y) + (MY1 * Z1.Y) + (MZ1 * Y1.Y));
+                double F0Z = (1.0 / restLength) * ((N * elementVec.Z) + (MY0 * Z0.Z) - (MZ0 * Y0.Z) + (MY1 * Z1.Z) + (MZ1 * Y1.Z)); 
+                Vector3d F0 = new Vector3d(F0X, F0Y, F0Z);          //Unit: [N]
+
+                //Force end
+                Vector3d F1 = -1 * F0;
+
+                //Moment start
+                //i=1, j=2, k=3
+                double M0X_pos = (-1.0) * (((MY0 * elementVec.Z * Z0.Y) / restLength) - ((MZ0 * elementVec.Z * Y0.Y) / restLength) + ((MX * ((Y0.Y * Z1.Z) - (Z0.Y * Y1.Z))) / 2.0));
+
+                //i=2, j=3, k=1
+                double M0Y_pos = (-1.0) * (((MY0 * elementVec.X * Z0.Z) / restLength) - ((MZ0 * elementVec.X * Y0.Z) / restLength) + ((MX * ((Y0.Z * Z1.X) - (Z0.Z * Y1.X))) / 2.0));
+
+                //i=3, j=1, k=2
+                double M0Z_pos = (-1.0) * (((MY0 * elementVec.Y * Z0.X) / restLength) - ((MZ0 * elementVec.Y * Y0.X) / restLength) + ((MX * ((Y0.X * Z1.Y) - (Z0.X * Y1.Y))) / 2.0));
+
+
+                //i=1, j=3, k=2
+                double M0X_neg = (1.0) * (((MY0 * elementVec.Y * Z0.Z) / restLength) - ((MZ0 * elementVec.Y * Y0.Z) / restLength) + ((MX * ((Y0.Z * Z1.Y) - (Z0.Z * Y1.Y))) / 2.0));
+
+                //i=2, j=1, k=3
+                double M0Y_neg = (1.0) * (((MY0 * elementVec.Z * Z0.X) / restLength) - ((MZ0 * elementVec.Z * Y0.X) / restLength) + ((MX * ((Y0.X * Z1.Z) - (Z0.X * Y1.Z))) / 2.0));
+
+                //i=3, j=2, k=1
+                double M0Z_neg = (1.0) * (((MY0 * elementVec.X * Z0.Y) / restLength) - ((MZ0 * elementVec.X * Y0.Y) / restLength) + ((MX * ((Y0.Y * Z1.X) - (Z0.Y * Y1.X))) / 2.0));
+
+                Vector3d M0 = new Vector3d(M0X_pos + M0X_neg, M0Y_pos + M0Y_neg, M0Z_pos + M0Z_neg);          //Unit: [Nm]
+
+
+                //Moment end
+                //i=1, j=2, k=3
+                double M1X_pos = (-1.0) * (((MY1 * elementVec.Z * Z1.Y) / restLength) - ((MZ1 * elementVec.Z * Y1.Y) / restLength) + ((MX * ((Y0.Y * Z1.Z) - (Z0.Y * Y1.Z))) / 2.0));
+
+                //i=2, j=3, k=1
+                double M1Y_pos = (-1.0) * (((MY1 * elementVec.X * Z1.Z) / restLength) - ((MZ1 * elementVec.X * Y1.Z) / restLength) + ((MX * ((Y0.Z * Z1.X) - (Z0.Z * Y1.X))) / 2.0));
+
+                //i=3, j=1, k=2
+                double M1Z_pos = (-1.0) * (((MY1 * elementVec.Y * Z1.X) / restLength) - ((MZ1 * elementVec.Y * Y1.X) / restLength) + ((MX * ((Y0.X * Z1.Y) - (Z0.X * Y1.Y))) / 2.0));
+
+
+                //i=1, j=3, k=2
+                double M1X_neg = (1.0) * (((MY1 * elementVec.Y * Z1.Z) / restLength) - ((MZ1 * elementVec.Y * Y1.Z) / restLength) + ((MX * ((Y0.Z * Z1.Y) - (Z0.Z * Y1.Y))) / 2.0));
+
+                //i=2, j=1, k=3
+                double M1Y_neg = (1.0) * (((MY1 * elementVec.Z * Z1.X) / restLength) - ((MZ1 * elementVec.Z * Y1.X) / restLength) + ((MX * ((Y0.X * Z1.Z) - (Z0.X * Y1.Z))) / 2.0));
+
+                //i=3, j=2, k=1
+                double M1Z_neg = (1.0) * (((MY1 * elementVec.X * Z1.Y) / restLength) - ((MZ1 * elementVec.X * Y1.Y) / restLength) + ((MX * ((Y0.Y * Z1.X) - (Z0.Y * Y1.X))) / 2.0));           
+
+                Vector3d M1 = new Vector3d(M1X_pos + M1X_neg, M1Y_pos + M1Y_neg, M1Z_pos + M1Z_neg);            //Unit: [Nm]
+
+
+                //Move and torque vectors
+                Move[0] = F0;
+                Move[1] = F1;
+                Torque[0] = M0;
+                Torque[1] = M1;
             }
 
 
-            //Output moment in [kNm] and stress in [MPa]
+            //Output moment in [kNm] and normal force/shear in [kN]
             public override object Output(List<KangarooSolver.Particle> p)
             {
                 List<object> DataOut = new List<object>();
 
+                //Updated local planes
                 DataOut.Add(P0R);
                 DataOut.Add(P1R);
-                DataOut.Add(TX1);
-                DataOut.Add(TX2);
-                DataOut.Add(TY1);
-                DataOut.Add(TY2);
-                DataOut.Add(twist);
+
+                //Element forces and moments
+                DataOut.Add(Math.Round(N * 1e-3, 3));           //Unit: [kN]
+                DataOut.Add(Math.Round(MY0 * 1e-3, 3));         //Unit: [kNm]
+                DataOut.Add(Math.Round(MZ0 * 1e-3, 3));         //Unit: [kNm]
+                DataOut.Add(Math.Round(MY1 * 1e-3, 3));         //Unit: [kNm]
+                DataOut.Add(Math.Round(MZ1 * 1e-3, 3));         //Unit: [kNm]
+                DataOut.Add(Math.Round(MX * 1e-3, 3));          //Unit: [kNm]
+
+
+                //Angles
+                /*
+                DataOut.Add(thetaX);
+                DataOut.Add(thetaY0);
+                DataOut.Add(thetaZ0);
+                DataOut.Add(thetaY1);
+                DataOut.Add(thetaZ1);
+                */
 
                 return DataOut;
 
-                //Create beam data object to store output information
-                //DataTypes.RodData rodData = new DataTypes.RodData(PIndex[1], pl, moment * 1e-6, bendingStress);
-                //return rodData;
+                //To do: create beam data object to store output information
+                //DataTypes.BeamData beamData = new DataTypes.BeamData();
+                //return beamData;
             }
 
         }
